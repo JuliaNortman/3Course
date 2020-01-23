d0e5609ad2b1a41f510a0974480a067c45bf6a96
Julia Nortman <ynortman@gmail.com>
2020-01-23 17:44:40 +0200

    Finish first part o fthe first lab

--- ParallelCounting/Lab1/src/com/knu/ynortman/SimpleThreadTask.java
+++ ParallelCounting/Lab1/src/com/knu/ynortman/SimpleThreadTask.java
@@ -2,12 +2,11 @@ package com.knu.ynortman;
 
 public class SimpleThreadTask {
     private Slider slider;
+    private Thread t1;
+    private Thread t2;
 
     public SimpleThreadTask() {
         slider = new Slider();
-    }
-
-    public void execute() {
         Runnable right = () -> {
             while(true) {
                 slider.moveOnePositionRight();
@@ -19,12 +18,44 @@ public class SimpleThreadTask {
                 slider.moveOnePositionLeft();
             }
         };
+        t1 = new Thread(right, "Thread1");
+        t2 = new Thread(left,  "Thread2");
+        slider.setLeftDecrActionListener(this::decrLeftPriority );
+        slider.setLeftIncrActionListener(this::incrLeftPriority);
+        slider.setRightDecrActionListener(this::decrRightPriority);
+        slider.setRightIncrActionListener(this::incrRightPriority);
+    }
 
-        Thread t1 = new Thread(right);
-        Thread t2 = new Thread(left);
+    public void execute() {
+
         t1.start();
         t2.start();
         /*You can set the thread priority here*/
         //t2.setPriority(8);
+    }
+
+    public synchronized void incrRightPriority() {
+        if(t1.getPriority() < 10) {
+            t1.setPriority(t1.getPriority() + 1);
+        }
+        slider.setRightLableText(Integer.toString(t1.getPriority()));
+    }
+    public synchronized void incrLeftPriority() {
+        if(t2.getPriority() < 10) {
+            t2.setPriority(t2.getPriority() + 1);
+        }
+        slider.setLeftLableText(Integer.toString(t2.getPriority()));
+    }
+    public synchronized void decrRightPriority() {
+        if(t1.getPriority() > 1) {
+            t1.setPriority(t1.getPriority() - 1);
+        }
+        slider.setRightLableText(Integer.toString(t1.getPriority()));
+    }
+    public synchronized void decrLeftPriority() {
+        if(t2.getPriority() > 1) {
+            t2.setPriority(t2.getPriority() - 1);
+        }
+        slider.setLeftLableText(Integer.toString(t2.getPriority()));
     }
 }
--- ParallelCounting/Lab1/src/com/knu/ynortman/Slider.java
+++ ParallelCounting/Lab1/src/com/knu/ynortman/Slider.java
@@ -4,6 +4,13 @@ import javax.swing.*;
 
 public class Slider {
     private JSlider slider;
+    private JButton incrLeft;
+    private JButton incrRight;
+    private JButton decrLeft;
+    private JButton decrRight;
+    private JLabel rightLabel;
+    private JLabel leftLabel;
+
 
     public Slider() {
         // Create and set up a frame window
@@ -14,9 +21,21 @@ public class Slider {
         JPanel panel = new JPanel();
         // Different settings on the sliders
         slider = new JSlider();
+        incrLeft = new JButton("+");
+        incrRight = new JButton("+");
+        decrLeft = new JButton("-");
+        decrRight = new JButton("-");
+        rightLabel = new JLabel();
+        leftLabel = new JLabel();
 
         // Add the slider to the panel
+        panel.add(leftLabel);
+        panel.add(incrLeft);
+        panel.add(decrLeft);
         panel.add(slider);
+        panel.add(incrRight);
+        panel.add(decrRight);
+        panel.add(rightLabel);
 
         // Set the window to be visible as the default to be false
         frame.add(panel);
@@ -27,16 +46,37 @@ public class Slider {
     public synchronized void  moveOnePositionRight() {
         slider.setValue(slider.getValue()+1);
         try {
-            Thread.currentThread().sleep(200);
+            Thread.sleep(200);
         }
-        catch (InterruptedException e) {}
+        catch (InterruptedException ignored) {}
     }
 
     public synchronized void moveOnePositionLeft() {
         slider.setValue(slider.getValue()-1);
         try {
-            Thread.currentThread().sleep(200);
+            Thread.sleep(200);
         }
-        catch (InterruptedException e) {}
+        catch (InterruptedException ignored) {}
+    }
+
+    public synchronized void setRightIncrActionListener(Runnable r) {
+        incrRight.addActionListener(actionEvent -> r.run());
+    }
+    public synchronized void setLeftIncrActionListener(Runnable r) {
+        incrLeft.addActionListener(actionEvent -> r.run());
+    }
+    public synchronized void setRightDecrActionListener(Runnable r) {
+        decrRight.addActionListener(actionEvent -> r.run());
+    }
+    public synchronized void setLeftDecrActionListener(Runnable r) {
+        decrLeft.addActionListener(actionEvent -> r.run());
+    }
+
+    public void setRightLableText(String text) {
+        rightLabel.setText(text);
+    }
+
+    public void setLeftLableText(String text) {
+        leftLabel.setText(text);
     }
 }
