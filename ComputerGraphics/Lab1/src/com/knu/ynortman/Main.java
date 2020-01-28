2e790b20225775963647775de59b9475d763b83f
Julia Nortman <ynortman@gmail.com>
2020-01-28 18:12:49 +0200

    Add vizualization

--- ComputerGraphics/Lab1/src/com/knu/ynortman/Main.java
+++ ComputerGraphics/Lab1/src/com/knu/ynortman/Main.java
@@ -1,27 +1,18 @@
 package com.knu.ynortman;
 
 import com.knu.ynortman.algorithm.Lab1Algo;
+import com.knu.ynortman.datastructure.Point;
 import com.knu.ynortman.view.WindowComponent;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class Main extends JFrame {
+public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         //System.out.println(Lab1Algo.execute());
-        Main app = new Main();
-        app.exec();
-    }
-
-    public void exec() {
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        WindowComponent comp = new WindowComponent(Lab1Algo.createPolygon());
-        comp.setPreferredSize(new Dimension(320, 200));
-        getContentPane().add(comp, BorderLayout.CENTER);
-        pack();
-        setVisible(true);
+        WindowComponent comp = new WindowComponent(Lab1Algo.createPolygon(), new Point(2, -2));
     }
 
 
--- ComputerGraphics/Lab1/src/com/knu/ynortman/view/WindowComponent.java
+++ ComputerGraphics/Lab1/src/com/knu/ynortman/view/WindowComponent.java
@@ -1,48 +1,67 @@
 package com.knu.ynortman.view;
 
 import com.knu.ynortman.datastructure.Edge;
+import com.knu.ynortman.datastructure.Point;
 import com.knu.ynortman.datastructure.Polygon;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 
-public class WindowComponent extends JComponent {
+public class WindowComponent extends JPanel {
     private Polygon polygon;
 
-    public WindowComponent(Polygon polygon) {
+    /*public WindowComponent(Polygon polygon) {
         this.polygon = polygon;
+    }*/
+
+    public WindowComponent(Polygon polygon, Point point) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        final int taskBarHeight = 40;
+        int screenHeight = screenSize.height - taskBarHeight;
+        int screenWidth = screenSize.width;
+        JFrame frame = new JFrame();
+        frame.setSize(new Dimension(screenWidth, screenHeight));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(){
+            Graphics2D g2;
+
+            protected void paintComponent(Graphics g){
+                super.paintComponent(g);
+                g2 = (Graphics2D) g;
+                g2.translate(screenWidth / 2,screenHeight / 2);
+                g2.scale(1.0, -1.0);
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                g.setColor(new Color(195,0,205));
+                g2.draw(new Line2D.Double(-1000,0,1000,0));
+                g2.draw(new Line2D.Double(0,-1000,0,1000));
+
+                ArrayList<Line2D> edges = transformEdges(polygon);
+                g2.setColor(Color.BLACK);
+                for (Line2D l : edges){
+                    g2.draw(l);
+                }
+                g2.setColor(Color.GREEN);
+                g2.drawOval((int)(point.getX()*10), (int)(point.getY()*10), 2, 2);
+            }
+        };
+
+        frame.setContentPane(panel);
+        frame.setVisible(true);
     }
 
-    private static final double SCALE_X = 20;
-
-    private static final double SCALE_Y = 15;
-
-    @Override
-    public void paint(Graphics grphcs) {
-        super.paint(grphcs);
-        Graphics2D g2 = (Graphics2D) grphcs;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        drawGraph(g2);
-    }
-
-    private void drawGraph(Graphics2D g2) {
-        double lastX = 0;
-        double lastY1 = Main.HEIGHT / 2;
-        double lastY2 = Main.HEIGHT / 2;
-        double lastY3 = Main.HEIGHT / 2;
-        for (int x = 1; x <= Main.WIDTH; x++) {
-            double y1 = Main.HEIGHT / 2 - func1(x / SCALE_X) * SCALE_Y;
-            double y2 = Main.HEIGHT / 2 - func2(x / SCALE_X) * SCALE_Y;
-            double y3 = Main.HEIGHT / 2 - func3(x / SCALE_X) * SCALE_Y;
-            g2.drawLine((int)lastX, (int)lastY1, (int)x, (int)y1);
-            g2.drawLine((int)lastX, (int)lastY2, (int)x, (int)y2);
-            g2.drawLine((int)lastX, (int)lastY3, (int)x, (int)y3);
-            lastX = x;
-            lastY1 = y1;
-            lastY2 = y2;
-            lastY3 = y3;
+    public ArrayList<Line2D> transformEdges(Polygon polygon) {
+        ArrayList<Line2D> edges = new ArrayList<>();
+        for(Edge e : polygon.getEdges()) {
+            edges.add(new Line2D.Double(e.getA().getX()*10, e.getA().getY()*10, e.getB().getX()*10, e.getB().getY()*10));
         }
+        return edges;
     }
+
    /* @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
