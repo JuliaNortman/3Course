cf6629ae61ae408c1335e9727f5d1709cf751ee8
Julia Nortman <ynortman@gmail.com>
2020-04-19 14:26:24 +0300

    Normalize table and fix some bugs

--- OOP/lab3/src/main/java/com/knu/ynortman/lab3/model/CrewMember.java
+++ OOP/lab3/src/main/java/com/knu/ynortman/lab3/model/CrewMember.java
@@ -3,12 +3,12 @@ package com.knu.ynortman.lab3.model;
 import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.validation.constraints.NotBlank;
 
@@ -25,7 +25,8 @@ public class CrewMember {
 	@NotBlank(message = "Person name is mandatory")
 	private String name;
 	
-	@Enumerated(EnumType.STRING)
+	@ManyToOne
+	@JoinColumn(name = "role_id", nullable = false)
 	private CrewRole role;
 	
 	@ManyToMany(mappedBy = "crewMembers")
--- OOP/lab3/src/main/java/com/knu/ynortman/lab3/model/CrewRole.java
+++ OOP/lab3/src/main/java/com/knu/ynortman/lab3/model/CrewRole.java
@@ -1,5 +1,23 @@
 package com.knu.ynortman.lab3.model;
 
-public enum CrewRole {
-	PILOT, NAVIGATOR, RADIO_OPERATOR, STEWARDESS 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class CrewRole {
+	@Id
+	@GeneratedValue(generator = "crewrole_id_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "crewrole_id_seq", sequenceName = "crewrole_id_seq", allocationSize = 5)
+	private int id;
+	
+	@Enumerated(EnumType.STRING)
+	private CrewRoleEnum role;
 }
