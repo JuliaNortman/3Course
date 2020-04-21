4ca336d15382e18ed9cc677010c162b56e9ebefa
Julia Nortman <ynortman@gmail.com>
2020-04-21 12:07:00 +0300

    Fix bugs

--- OOP/Lab1/src/main/java/hedgehoggame/gamelogic/Game.java
+++ OOP/Lab1/src/main/java/hedgehoggame/gamelogic/Game.java
@@ -3,36 +3,38 @@ package hedgehoggame.gamelogic;
 import java.util.Arrays;
 import java.util.Random;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
 public class Game {
 	private final Cell[] field; //game field
-	private final int N; //x dimension
-	private final int M; //y dimension
+	private final int width; //x dimension
+	private final int height; //y dimension
 	private boolean gameEnd; //true if game is over
 	private int appleNum; //number of apples available
 
-	private Pair<Integer, Integer> hedgehogPosition;
+	private ImmutablePair<Integer, Integer> hedgehogPosition;
 	
 	public Game(int xDimension, int yDimension) {
-		this.N = xDimension;
-		this.M = yDimension;
-		field = new Cell[N*M];
+		this.width = xDimension;
+		this.height = yDimension;
+		field = new Cell[width*height];
 	}
 	
 	public Cell[] getField() {
 		return this.field.clone();
 	}
 	
-	public Pair<Integer, Integer> getHedgehogPosition() {
+	public ImmutablePair<Integer, Integer> getHedgehogPosition() {
 		return this.hedgehogPosition;
 	}
 	
 
-	public int getN() {
-		return N;
+	public int getWidth() {
+		return width;
 	}
 
-	public int getM() {
-		return M;
+	public int getHeight() {
+		return height;
 	}
 	
 	public boolean getGameEnd() {
@@ -46,15 +48,15 @@ public class Game {
 	public void init() {
 		Arrays.fill(field, Cell.EMPTY);
 		
-		setCell(Cell.HEDGEHOG, M-1, 0);
-		hedgehogPosition = new Pair<Integer, Integer>(M-1, 0);
-		int density = N*M/5;
+		setCell(Cell.HEDGEHOG, height-1, 0);
+		hedgehogPosition = new ImmutablePair<Integer, Integer>(height-1, 0);
+		int density = width*height/5;
 		for(int k = 0; k < density; ++k) {
-			int j = new Random().nextInt(N);
-			int i = new Random().nextInt(M);		
+			int j = new Random().nextInt(width);
+			int i = new Random().nextInt(height);		
 			while(getCell(i, j) != Cell.EMPTY) {
-				j = new Random().nextInt(N);
-				i = new Random().nextInt(M);		
+				j = new Random().nextInt(width);
+				i = new Random().nextInt(height);		
 			}
 			setCell(Cell.APPLE, i, j);
 			appleNum++;
@@ -62,13 +64,13 @@ public class Game {
 	}
 	
 	public int getCellIndex(int i, int j) {
-		if(i < 0 || i > M-1) {
+		if(i < 0 || i > height-1) {
 			throw new IllegalArgumentException("Index i=" + i + " is out of array bounds");
 		}
-		if(j < 0 || j > N-1) {
+		if(j < 0 || j > width-1) {
 			throw new IllegalArgumentException("Index j=" + j + " is out of array bounds");
 		}
-		return i*N + j;
+		return i*width + j;
 	}
 	
 	public void setCell(Cell cell, int i, int j) {
@@ -87,8 +89,8 @@ public class Game {
 	}
 	
 	public void moveTop() {
-		int curX = hedgehogPosition.getKey();
-		int curY = hedgehogPosition.getValue();
+		int curX = hedgehogPosition.getLeft();
+		int curY = hedgehogPosition.getRight();
 		
 		if(curX != 0) {
 			setCell(Cell.EMPTY, curX, curY);
@@ -96,7 +98,7 @@ public class Game {
 				decreaseAppleNum();
 			}
 			setCell(Cell.HEDGEHOG, curX-1, curY);
-			hedgehogPosition = new Pair<Integer, Integer>(curX-1, curY);
+			hedgehogPosition = new ImmutablePair<Integer, Integer>(curX-1, curY);
 		}
 	}
 	
@@ -104,13 +106,13 @@ public class Game {
 		int curX = hedgehogPosition.getKey();
 		int curY = hedgehogPosition.getValue();
 		
-		if(curX != M-1) {
+		if(curX != height-1) {
 			if(getCell(curX+1, curY) == Cell.APPLE) {
 				decreaseAppleNum();
 			}
 			setCell(Cell.EMPTY, curX, curY);
 			setCell(Cell.HEDGEHOG, curX+1, curY);
-			hedgehogPosition = new Pair<Integer, Integer>(curX+1, curY);
+			hedgehogPosition = new ImmutablePair<Integer, Integer>(curX+1, curY);
 		}
 	}
 	
@@ -124,7 +126,7 @@ public class Game {
 			}
 			setCell(Cell.EMPTY, curX, curY);
 			setCell(Cell.HEDGEHOG, curX, curY-1);
-			hedgehogPosition = new Pair<Integer, Integer>(curX, curY-1);
+			hedgehogPosition = new ImmutablePair<Integer, Integer>(curX, curY-1);
 		}
 	}
 	
@@ -132,13 +134,13 @@ public class Game {
 		int curX = hedgehogPosition.getKey();
 		int curY = hedgehogPosition.getValue();
 		
-		if(curY != N-1) {
+		if(curY != width-1) {
 			if(getCell(curX, curY+1) == Cell.APPLE) {
 				decreaseAppleNum();
 			}
 			setCell(Cell.EMPTY, curX, curY);
 			setCell(Cell.HEDGEHOG, curX, curY+1);
-			hedgehogPosition = new Pair<Integer, Integer>(curX, curY+1);
+			hedgehogPosition = new ImmutablePair<Integer, Integer>(curX, curY+1);
 		}
 	}
 	
