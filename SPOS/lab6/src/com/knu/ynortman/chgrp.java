c9b8b5c0395a71f0d6fa1917c7624a16da16b917
Julia Nortman <ynortman@gmail.com>
2020-05-11 19:40:57 +0300

    Add chown and chgrp

---
+++ SPOS/lab6/src/com/knu/ynortman/chgrp.java
@@ -0,0 +1,43 @@
+package com.knu.ynortman;
+
+public class chgrp {
+    public static final String PROGRAM_NAME = "chgrp" ;
+
+    public static void main(String[] args) throws Exception {
+
+        //initialize simulator kernel
+        Kernel.initialize();
+
+        if( args.length < 2 )
+        {
+            System.err.println( PROGRAM_NAME + ": too few arguments" ) ;
+            Kernel.exit( 1 ) ;
+        }
+
+        short gid = Short.parseShort(args[0]);
+
+        if (gid < 0)
+        {
+            Kernel.perror( PROGRAM_NAME ) ;
+            System.err.println( PROGRAM_NAME + ": incorrect gid " ) ;
+            Kernel.exit( 2 ) ;
+        }
+
+
+        // for each filename specified
+        for( int i = 1 ; i < args.length ; i ++ )
+        {
+            String name = args[i] ;
+
+            int res = Kernel.chown(name, (short) -1, gid) ;
+            if( res < 0 )
+            {
+                Kernel.perror( PROGRAM_NAME ) ;
+                System.err.println( PROGRAM_NAME + ": cannot to change group ownership of the file \"" + name + "\"" ) ;
+                Kernel.exit( 3 ) ;
+            }
+        }
+
+        Kernel.exit(0);
+    }
+}
---
+++ SPOS/lab6/src/com/knu/ynortman/chown.java
@@ -0,0 +1,43 @@
+package com.knu.ynortman;
+
+public class chown {
+    public static final String PROGRAM_NAME = "chown" ;
+
+    public static void main(String[] args) throws Exception {
+
+        //initialize simulator kernel
+        Kernel.initialize();
+
+        if( args.length < 2 )
+        {
+            System.err.println( PROGRAM_NAME + ": too few arguments" ) ;
+            Kernel.exit( 1 ) ;
+        }
+
+        short uid = Short.parseShort(args[0]);
+
+        if (uid < 0)
+        {
+            Kernel.perror( PROGRAM_NAME ) ;
+            System.err.println( PROGRAM_NAME + ": incorrect uid " ) ;
+            Kernel.exit( 2 ) ;
+        }
+
+
+        // for each filename specified
+        for( int i = 1 ; i < args.length ; i ++ )
+        {
+            String name = args[i] ;
+
+            int res = Kernel.chown(name, uid, (short) -1) ;
+            if( res < 0 )
+            {
+                Kernel.perror( PROGRAM_NAME ) ;
+                System.err.println( PROGRAM_NAME + ": cannot to change owner of the file \"" + name + "\"" ) ;
+                Kernel.exit( 3 ) ;
+            }
+        }
+
+        Kernel.exit(0);
+    }
+}
